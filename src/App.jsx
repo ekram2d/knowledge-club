9d5f13800d5c7971f95db45c91c41aafd79e910f
Ekram Hossain <ekramhossain117@gmail.com>
2023-04-01 09:25:02 +0600

    fetch the data using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Nabvar from './Components/Nabvar/Nabvar';
 
 const App = () => {
-
+   const [data,userData]=useState([]);
+   useEffect(()=>{
+      fetch("data.json")
+      .then(res=>res.json())
+      .then ((data)=>console.log(data));
+   },[])
   
   return (
     <>
